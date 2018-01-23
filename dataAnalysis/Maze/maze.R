377b3c0cdee38a88b92a2c28e67759a66c24ec84
Sue Cheng <nuannuan7@gmail.com>
2018-01-24 12:09:07 +1300

    plot data in R, 1)calc by using dplyr 2)added axis line

--- dataAnalysis/Maze/maze.R
+++ dataAnalysis/Maze/maze.R
@@ -4,24 +4,62 @@ library(ggplot2)
 
 
 ############ settings ##############
-maze4 <- c('maze4_path_weight100_weight30.csv','Maze4 - 201801162151 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv') #Maze4 - 201801162151 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv')
+#maze4 <- c('maze4_path_weight100_weight30.csv','Maze4 - 201801162151 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv') #Maze4 - 201801162151 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv')
+maze4 <- c('maze4_path_weight100_weight30.csv','nxcs.testbed.maze4_weighted_sum - 201801222157 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv')
 maze5 <- c('maze5_path_weight100_weight30.csv','Maze5- 201801162156 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv') #Maze5 - 201801162151 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv')
 maze6 <- c('maze6_path_weight100_weight30.csv','Maze6 - 201801161300 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv') #Maze6 - 201801151104 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv.csv')
 
 
 upperBound <- 6000
-traceWeightFilter <- c('0.040000|0.960000'
-                       , '0.480000|0.520000'
-                       #, '0.520000|0.480000'
-                       , '0.960000|0.040000') #c('0.000000|1.000000', '0.560000|0.440000', '1.000000|0.000000')
+# traceWeightFilter <- c('0.040000|0.960000'
+#                        , '0.480000|0.520000'
+#                        #, '0.520000|0.480000'
+#                        , '0.960000|0.040000') #c('0.000000|1.000000', '0.560000|0.440000', '1.000000|0.000000')
+# 
+# plot.labels <- list(expression(paste(lambda[1],'=0.04, 0.96','  ',sep=''))
+#                     , expression(paste(lambda[2],'=0.48, 0.52','  ',sep=''))
+#                     #, expression(paste(lambda[3],'=0.52, 0.48',sep=''))
+#                     , expression(paste(lambda[3],'=0.96, 0.04','  ',sep='')) )
 
-plot.labels <- list(expression(paste(lambda[1],'=0.04, 0.96','  ',sep=''))
-                    , expression(paste(lambda[2],'=0.48, 0.52','  ',sep=''))
-                    #, expression(paste(lambda[3],'=0.52, 0.48',sep=''))
-                    , expression(paste(lambda[3],'=0.96, 0.04','  ',sep='')) )
+
+traceWeightFilter <- c('0.000000|1.000000' ,
+                       '0.111111|0.888889' ,
+                       '0.222222|0.777778' ,
+                       '0.333333|0.666667' ,
+                       '0.444444|0.555556' ,
+                       '0.555556|0.444444' ,
+                       '0.666667|0.333333' ,
+                       '0.777778|0.222222' ,
+                       '0.888889|0.111111' ,
+                       '1.000000|0.000000'
+                       )
+
+plot.upperBound <- 3000
+plot.traceWeightFilter <- c('0.000000|1.000000' ,
+                       '0.111111|0.888889' ,
+                       '0.222222|0.777778' ,
+                       '0.333333|0.666667' ,
+                       '0.444444|0.555556' ,
+                       '0.555556|0.444444' ,
+                       '0.666667|0.333333' ,
+                       '0.777778|0.222222' ,
+                       '0.888889|0.111111' ,
+                       '1.000000|0.000000')
+
+plot.labels <- list(expression(paste(lambda[0],'=0.0, 1.0','  ',sep=''))
+                    , expression(paste(lambda[1],'=0.11, 0.89','  ',sep=''))
+                    , expression(paste(lambda[2],'=0.22, 0.78','  ',sep=''))
+                    , expression(paste(lambda[3],'=0.33, 0.67','  ',sep=''))
+                     , expression(paste(lambda[4],'=0.44, 0.56','  ',sep=''))
+                    , expression(paste(lambda[5],'=0.56, 0.44','  ',sep=''))
+                    , expression(paste(lambda[6],'=0.67, 0.33','  ',sep=''))
+                    , expression(paste(lambda[7],'=0.78, 0.22','  ',sep=''))
+                    , expression(paste(lambda[8],'=0.89, 0.11','  ',sep=''))
+                    , expression(paste(lambda[9],'=1.0, 0.0','  ',sep='')) )
+
 
 ##################
-mazeToRun <- maze6
+mazeToRun <- maze4
 
 ############# begin to read result #############
 setwd("C:/Users/martin.xie/IdeaProjects/XCS_MOEAD/dataAnalysis/Maze")
@@ -34,12 +72,19 @@ targetSteps <- cbind(targetSteps, targetId)
 setwd("C:/Users/martin.xie/IdeaProjects/XCS_MOEAD/log/MOXCS")
 raw.data <- read.csv(file =   mazeToRun[2] #Train - 201801141417 - Trial 0 - TRIAL_NUM - 6000 - TEST.csv.csv"
                      , header = TRUE, sep = ","
-                     , stringsAsFactors = FALSE)
+                     , stringsAsFactors = FALSE
+                     , row.names=NULL)
+
+ax <- colnames(raw.data)
+
+raw.data$PAtotal3 <- NULL
+
+names(raw.data) <- ax[-1]
 
 
 data <- raw.data %>% 
     select(TrailNumber, Timestamp, TargetWeight, TraceWeight, obj_r1, OpenState, FinalState, steps, hyperVolumn, path) %>%
-    #filter(TraceWeight == ' 0.000000|1.000000')
+      #filter(TraceWeight == ' 0.000000|1.000000')
     #%>% filter(TrailNumber == 0)
     filter(TraceWeight %in% traceWeightFilter
           , Timestamp <= upperBound)
@@ -49,42 +94,19 @@ data <- raw.data %>%
 #unique(data$TraceWeight)
 
 
-uid <- paste(trimws(data$OpenState), trimws(data$FinalState), data$steps, sep = "*")
+uid <- paste(data$OpenState, data$FinalState, data$steps, sep = "*")
 data <- cbind(data, uid)
+data$match <- ifelse(data$uid %in% targetSteps$targetId, 1, 0)
 rm(uid)
 
 result <- data %>%
-            select(TrailNumber, Timestamp,TargetWeight,TraceWeight,hyperVolumn ) %>%distinct
+            group_by(TrailNumber, Timestamp,TargetWeight,TraceWeight ) %>%
+            summarise(groupRow = n()
+                    , matchCount = sum(match)
+                    , matchRate =matchCount/groupRow 
+                    , hyperVolumn = mean(hyperVolumn))
 
  
-getMatchCountForRow <- function(arow, data) {
-    trows <- data %>% 
-            filter(TrailNumber == arow$TrailNumber
-                        ,Timestamp == arow$Timestamp
-                        , TargetWeight == arow$TargetWeight
-                        , TraceWeight == arow$TraceWeight
-                        )
-    #print(nrow(trows))
-    match <- 0
-    for (i in 1:nrow(trows)) {
-        #print(trows[i,]$uid)
-        match <- match + ifelse(trows[i,]$uid %in% targetSteps$targetId, 1, 0)
-    }
-    #print(paste(arow$TrailNumber, ':', arow$Timestamp, ':'
-    #            , arow$TargetWeight, ':'
-    #            , arow$TraceWeight, ':'
-    #            , match, '/', nrow(trows), sep = ' '))
-    (match/nrow(trows))
-}
-
-#match.rate <- getMatchCountForRow(result[1800,], data)
-
-#lapply(result, getMatchCountForRow, data)
-result$matchRate <- 0
-for (i in 1:nrow(result)) {
-    #print(getMatchCountForRow(result[i,], data))
-    result[i,]$matchRate <- getMatchCountForRow(result[i,], data)
-}
 
 ##result[1,]$matchRate <- getMatchCountForRow(result[1,], data)
 
@@ -98,9 +120,9 @@ for (i in uniqTrail) {
         #, TraceWeight == '5.000000|5.000000'
         #, TraceWeight == uniqWeight[i] #' 0.000000|1.000000'
         )
-    plt <- ggplot(pdata, aes(x = Timestamp, y = matchRate, group = TraceWeight, color = TraceWeight, linetype = TraceWeight)) +
-        geom_line()
-    print(plt)
+    ggplot(pdata, aes(x = Timestamp, y = matchRate, group = TraceWeight, color = TraceWeight, linetype = TraceWeight) )+
+      geom_line() +
+      labs(title = paste('Trail', i,sep=' ')) 
 }
 
 #ggplot(pdata, aes(x = Timestamp, y = matchRate, group = TraceWeight, color = c('#41ae76', '#ef6548', '#4292c6'))) +
@@ -113,23 +135,10 @@ for (i in uniqTrail) {
 #    summarise(avgmr = mean(matchRate))
 
 retdata <- result %>%
-  group_by(Timestamp, TargetWeight, TraceWeight) %>% distinct
-
-
-getMatchCountForRow_avg <- function(arow, data) {
-  trows <- data %>%
-    filter(Timestamp == arow$Timestamp, TargetWeight == arow$TargetWeight, TraceWeight == arow$TraceWeight)
-  
-  mean(trows$matchRate)
-}
-
-
-getMatchCountForRow_avgHV <- function(arow, data) {
-  trows <- data %>%
-    filter(Timestamp == arow$Timestamp, TargetWeight == arow$TargetWeight, TraceWeight == arow$TraceWeight)
-  
-  mean(trows$hyperVolumn)
-}
+  group_by(Timestamp, TargetWeight, TraceWeight) %>%
+  summarise(
+        matchRate = mean(matchRate) 
+          , hyperVolumn = mean(hyperVolumn))
 
 
 
@@ -137,28 +146,8 @@ getMatchCountForRow_avgHV <- function(arow, data) {
 #trows <- result %>%
 #            filter(Timestamp == arow$Timestamp, TargetWeight == arow$TargetWeight, TraceWeight == arow$TraceWeight)
 
-avgRate <- rep(0,nrow(retdata))
-for (i in 1:nrow(retdata)) {
-  #print(getMatchCountForRow_avg(retdata[i,], result))
-  avgRate[i] <- getMatchCountForRow_avg(retdata[i,], result)
-}
-
-hyperVolumn <- rep(0,nrow(retdata))
-for (i in 1:nrow(retdata)) {
-  #print(getMatchCountForRow_avg(retdata[i,], result))
-  hyperVolumn[i] <- getMatchCountForRow_avgHV(retdata[i,], result)
-}
-
-getMatchCountForRow_avg(retdata[2,], result)
-
-retdata$matchRate <- avgRate
-retdata$hyperVolumn <- hyperVolumn
-
-plt <- ggplot(retdata, aes(x = Timestamp, y = matchRage, group = TraceWeight, color = TraceWeight, linetype = TraceWeight)) +
+plt <- ggplot(retdata, aes(x = Timestamp, y = matchRate, group = TraceWeight, color = TraceWeight, linetype = TraceWeight)) +
     geom_line()
-
-ggplot(retdata, aes(x = Timestamp, y = hyperVolumn, group = TraceWeight, color = TraceWeight, linetype = TraceWeight)) +
-geom_line()
 
 
 
@@ -166,14 +155,18 @@ geom_line()
 theme_set(theme_classic(base_size = 9))
 
 
-lty = c(1, 2, 3, 4, 5, 6, 8, 9, 1)
-lshp = c(1, 2, 3, 4, 5, 6, 7, 8, 9)
-cbbPalette = c('#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#66ff66', '#a65628', '#f781bf', '#000000')
+lty = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+lshp = c(1, 2, 3, 4, 5, 6, 7, 8, 9,10)
+cbbPalette = c('#e41a1c', '#377eb8', '#4daf4a'
+               , '#984ea3', '#ff7f00', '#66ff66'
+               , '#a65628', '#f781bf', '#000000'
+               ,'#f781bf')
 
 
 plot.data <- retdata %>% filter(TraceWeight  %in% plot.traceWeightFilter
                                 , Timestamp <= plot.upperBound
-)
+                                , TargetWeight %in% c('65.000000|65.000000')
+            )
 
 #%>%filter(TraceWeight=='0.480000|0.520000',Timestamp<500,Timestamp>200)
 phv <- ggplot(data = plot.data, aes(
@@ -197,10 +190,12 @@ theme(legend.position = 'bottom') + theme(panel.grid.major = element_line(size =
   theme(legend.background = element_rect(fill = alpha('gray', 0.05))) +
   theme(axis.text.x = element_text(size = rel(1.4)),
         axis.text.y = element_text(size = rel(1.4)),
+        axis.line.x = element_line(size = rel(0.4),colour = 'black',linetype = 'solid'),
+        axis.line.y = element_line(size = rel(0.4),colour = 'black',linetype = 'solid'),
         axis.title = element_text(size = rel(1.2), face = "bold")) +
   scale_linetype_manual(values = lty, guide = "none") +
   scale_colour_manual(values = cbbPalette, labels = plot.labels) +
-  guides(colour=guide_legend(override.aes=list(linetype=1:3)))
+  guides(colour=guide_legend(override.aes=list(linetype=1:10)))
 
  
 pmr <- ggplot(data = plot.data, aes(
@@ -223,6 +218,8 @@ theme(legend.position = 'bottom') + theme(panel.grid.major = element_line(size =
   theme(legend.background = element_rect(fill = alpha('gray', 0.05))) +
   theme(axis.text.x = element_text(size = rel(1.4)),
         axis.text.y = element_text(size = rel(1.4)),
+        axis.line.x = element_line(size = rel(0.4),colour = 'black',linetype = 'solid'),
+        axis.line.y = element_line(size = rel(0.4),colour = 'black',linetype = 'solid'),
         axis.title = element_text(size = rel(1.2), face = "bold")) +
   scale_linetype_manual(values = lty) +
   scale_colour_manual(values = cbbPalette)
